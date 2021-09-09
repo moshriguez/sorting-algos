54b8d35a6d923d2330d31fad5bbb297a8dd3fc39
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-09 16:55:25 -0700

    renamed selection sort and added merge sort

---
+++ merge-sort.js
@@ -0,0 +1,30 @@
+function findMinAndRemove(firstHalf, secondHalf){
+	let minfirstHalf = firstHalf[0]
+	let minsecondHalf = secondHalf[0]
+
+	if(minfirstHalf < minsecondHalf){
+		return firstHalf.shift()
+	} else {
+		return secondHalf.shift()
+	}
+}
+
+function merge(firstHalf, secondHalf){
+	let sorted = []
+	while(firstHalf.length != 0 && secondHalf.length != 0){
+		let currentMin = findMinAndRemove(firstHalf, secondHalf)
+		sorted.push(currentMin)
+	}
+	return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array) {
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    if (array.length < 1) {
+        return array
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
